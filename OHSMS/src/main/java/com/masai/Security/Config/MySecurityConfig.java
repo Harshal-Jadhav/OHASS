ae906323210b10f4b3f9ee02d60de352601a1cc3
Harshal-Jadhav <harshaljadhav1000@gmail.com>
2022-12-31 12:51:12 +0530

    Security layer completed

--- OHSMS/src/main/java/com/masai/Security/Config/MySecurityConfig.java
+++ OHSMS/src/main/java/com/masai/Security/Config/MySecurityConfig.java
@@ -2,8 +2,10 @@ package com.masai.Security.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class MySecurityConfig {
@@ -11,5 +13,13 @@ public class MySecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain securityConfig(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth.antMatchers("/demo/addUsers", "/demo/addAdmin").hasAuthority("ADMIN")
+				.antMatchers("/demo/getAccount").hasAuthority("USER").antMatchers("get-all-users").permitAll()).csrf()
+				.disable().httpBasic();
+		return http.build();
 	}
 }
